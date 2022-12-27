75ca9840cb0cb9118a60d83b61c3584a39ad68d5
K18Tatiana <tatianavegac2003@gmail.com>
2022-12-27 01:56:59 -0500

    finish work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import UserCard from './components/UserCard'
+import users from "./data/users.json"
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const colors = ["#FDB137", "#785964", "#6D6875", "#B5838D", "#E5989B", "#7E9680", "#C73866", "#FFB4A2", "#79616F", "#EAB595" ]
+
+  const randomIndexUsers = Math.floor( Math.random() * users.length )
+  const randomIndexColors = Math.floor( Math.random() * colors.length )
+
+  const [ indexUser, setIndexUser ] = useState(randomIndexUsers)
+  const [ color, setColor ] = useState(colors[randomIndexColors])
+
+  document.body.style.backgroundColor = color
+
+  const changeIndex = () => {
+    const newIndexUsers = Math.floor( Math.random() * users.length )
+    setIndexUser(newIndexUsers)
+  }
+
+  useEffect( () => {
+    const newIndexColors = Math.floor( Math.random() * colors.length )
+    const newColor = colors[newIndexColors]
+    setColor(newColor)
+  }, [ indexUser ] )
+  
   return (
     <div className="App">
-      
+      <UserCard 
+        userData = { users[indexUser] }
+        randomColor = { color }
+      />
+      <button onClick={ () => changeIndex() }><i className="fa-solid fa-shuffle"></i></button>
     </div>
   )
 }
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+const UserCard = ( {userData, randomColor} ) => {
+    const { name, picture, email, phone, location } = userData
+    document.body.style.backgroundColor = randomColor
+    return (
+        <div className="user-card">
+            <h1 style={{color: randomColor}}>{name.title} {name.first} {name.last}</h1>
+            <img src={picture.large} alt=""/>
+            <ul style={{color: randomColor}}>
+                <li><i className="fa-solid fa-envelope"></i>{email}</li>
+                <li><i className="fa-solid fa-phone"></i>{phone}</li>
+                <li><i className="fa-solid fa-location-dot"></i>{location.country} {location.state} {location.city}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default UserCard
